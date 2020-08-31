aac443d0de86d23bc105956332466d8be1cefa48
Eduardo <dudarenz@gmail.com>
2020-08-30 23:22:14 -0300

    (refactor) dateIsLoweThan fn on utils

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,10 @@
      * Return HH:MM:SS date from date object
     **/
      export const getFormatedTime = (date:Date) =>{
+        if(!date)
+            return null
         return ` ${String(date.getHours()).padStart(2,"0")}:${String(date.getMinutes()).padStart(2,"0")}:${String(date.getUTCSeconds()).padStart(2,"0")} `
-    }
- 
+     }
+    export const dateIsLowerThan = (main_date:Date,date_to_compare:Date) => main_date < date_to_compare;
     
    
