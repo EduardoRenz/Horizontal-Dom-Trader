2a61176b32567b98319177326a967f7c40750ef5
Eduardo <dudarenz@gmail.com>
2020-09-14 03:13:15 -0300

    (chore) created labels

--- src/components/Charts/Renkko.ts
+++ src/components/Charts/Renkko.ts
@@ -4,18 +4,34 @@ export class Renkko {
     canvas:HTMLCanvasElement 
     ctx:CanvasRenderingContext2D
     Xlabels:string[]
+    Ylabels:number[]
+    RIGHT_OFFSET: number = 20
+    BOTTOM_OFFSET: number = 2
     constructor(canvas){
         this.canvas = canvas
         this.ctx =  canvas.getContext("2d");
     }
+
+
 
     drawXAxis(labels:string[]){
         let numbs = 0
         for (const label of labels.splice(0,4)) {
             this.ctx.font = "12px Roboto"
             this.ctx.fillStyle = "white"
-            this.ctx.fillText(label, numbs, 90);
+            this.ctx.fillText(label, numbs, this.canvas.height -this.BOTTOM_OFFSET);
             numbs+=80
+        }
+    }
+
+    drawYAxis(prices:number[]){
+        console.log(prices)
+        let numbs = 20
+        for (const price of prices.splice(0,4)) {
+            this.ctx.font = "12px Roboto"
+            this.ctx.fillStyle = "white"
+            this.ctx.fillText(price.toFixed(1), this.canvas.width - this.RIGHT_OFFSET, numbs);
+            numbs+=30
         }
     }
 
@@ -23,7 +39,10 @@ export class Renkko {
     update(agressions:IAgression[]){
         this.clear()
         this.Xlabels = agressions.map(agression=>getFormatedTime(agression.time))
+        this.Ylabels = agressions.map(agression=>agression.price).sort((a,b)=>b-a)
+        this.Ylabels = Array.from(new Set(this.Ylabels))
         this.drawXAxis(this.Xlabels)
+        this.drawYAxis(this.Ylabels)
 
     }
 
