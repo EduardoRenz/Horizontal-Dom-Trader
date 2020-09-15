58ffeaeb96c4a348fa6202bc1f0c027f660f8524
Eduardo <dudarenz@gmail.com>
2020-09-15 18:33:14 -0300

    trying canvas html

--- src/components/Charts/Renkko.svelte
+++ src/components/Charts/Renkko.svelte
@@ -2,7 +2,7 @@
   import { agressions } from "../../store";
   import { getFormatedTime } from "../../utils";
   const TIMES_TO_SHOW = 5;
-  const PRICES_TO_SHOW = 4;
+  const PRICES_TO_SHOW = 5;
   $: times = $agressions.map((agression) => agression.time);
   $: prices = $agressions.map((agression) => agression.price);
 </script>
@@ -31,31 +31,53 @@
     justify-content: space-between;
     align-items: center;
   }
-  .prices {
+  .prices-wrapper {
     grid-area: prices;
     display: flex;
     flex-direction: column;
     width: auto;
     align-items: flex-end;
     justify-content: space-around;
+    overflow: hidden;
+  }
+  .prices {
+    display: flex;
+    flex-direction: column;
+    height: 1000px;
+    gap: 10px;
+    align-items: center;
   }
   .candles {
     grid-area: candles;
+    display: flex;
+    gap:2px;
+  }
+
+  .candle {
+    height: 20px;
+    width: 10px;
+    background: green;
   }
 </style>
 
 <div class="renkko">
-  <div class="prices">
-    {#each prices.splice(0, PRICES_TO_SHOW).sort((a, b) => b - a) as price,i}
-      <span>{price.toFixed(1)}</span>
-    {/each}
-  </div>
-  <div class="candles">ca</div>
+  <div class="prices-wrapper">
+    <div class="prices">
+      {#each prices.splice(0, PRICES_TO_SHOW).sort((a, b) => b - a) as price, i}
+        <span>{price.toFixed(1)}</span>
+      {/each}
+    </div>
 
+  </div>
+  <div class="candles">
+    <!-- {#each $agressions.splice(0,15) as agression,i}
+    <div class="candle" style={`background:${$agressions[i-1] && $agressions[i-1].price > agression.price ? 'red' : 'green' }`} />
+    {/each} -->
+  </div>
   <div class="times">
     {#each times.splice(0, TIMES_TO_SHOW) as time}
       <span>{getFormatedTime(time)}</span>
     {/each}
-    <span></span>
+    <span />
   </div>
 </div>
