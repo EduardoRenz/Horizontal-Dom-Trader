cbb4464dd8851958b8ecb5850f35998c0838205f
Eduardo <dudarenz@gmail.com>
2020-09-18 02:30:00 -0300

    (chore) get price range with a function

--- src/components/Charts/Renkko.svelte
+++ src/components/Charts/Renkko.svelte
@@ -2,9 +2,20 @@
   import { agressions } from "../../store";
   import { getFormatedTime } from "../../utils";
   const TIMES_TO_SHOW = 5;
-  const PRICES_TO_SHOW = 5;
+  const PRICES_TO_SHOW = 50;
   $: times = $agressions.map((agression) => agression.time);
-  $: prices = $agressions.map((agression) => agression.price);
+
+  function getPriceRange(){
+    const min_price =  $agressions.map((agression) => agression.price).reduce((a,b)=> a<b ? a : b)
+    const max_price =  $agressions.map((agression) => agression.price).reduce((a,b)=> a>b ? a : b)
+    let range = []
+    for (let index = max_price; index >= min_price; index-=0.5) {
+      range.push(index)
+    }
+    console.log(range)
+    return range
+  }
+
 </script>
 
 <style>
@@ -23,6 +34,13 @@
       "times times";
     grid-template-rows: 1fr 15px;
     grid-template-columns: 1fr 40px;
+    overflow-y: scroll;
+  }
+  .time-wrapper{
+    grid-area: times;
+    position: absolute;
+    bottom: 10px;
+    width: inherit;
   }
   .times {
     grid-area: times;
@@ -30,20 +48,15 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-right: 20px;
   }
-  .prices-wrapper {
+
+
+  .prices {
     grid-area: prices;
     display: flex;
     flex-direction: column;
-    width: auto;
-    align-items: flex-end;
-    justify-content: space-around;
-    overflow: hidden;
-  }
-  .prices {
-    display: flex;
-    flex-direction: column;
-    height: 1000px;
+
     gap: 10px;
     align-items: center;
   }
@@ -54,30 +67,31 @@
   }
 
   .candle {
-    height: 20px;
-    width: 10px;
+    height: 10px;
+    width: 5px;
     background: green;
   }
 </style>
 
 <div class="renkko">
-  <div class="prices-wrapper">
     <div class="prices">
-      {#each prices.splice(0, PRICES_TO_SHOW).sort((a, b) => b - a) as price, i}
+      {#each getPriceRange() as price, i}
         <span>{price.toFixed(1)}</span>
       {/each}
     </div>
 
-  </div>
   <div class="candles">
-    <!-- {#each $agressions.splice(0,15) as agression,i}
+    {#each Array.from($agressions).reverse() as agression,i}
     <div class="candle" style={`background:${$agressions[i-1] && $agressions[i-1].price > agression.price ? 'red' : 'green' }`} />
-    {/each} -->
-  </div>
-  <div class="times">
-    {#each times.splice(0, TIMES_TO_SHOW) as time}
-      <span>{getFormatedTime(time)}</span>
     {/each}
-    <span />
   </div>
+  <div class="time-wrapper">
+    <div class="times">
+      {#each times.splice(0, TIMES_TO_SHOW) as time}
+        <span>{getFormatedTime(time)}</span>
+      {/each}
+      <span />
+    </div>
+  </div>
+
 </div>
