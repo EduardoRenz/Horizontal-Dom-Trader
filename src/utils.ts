aa8429b2a95c46fcb43d238cd66df49b34fb6d30
Eduardo <dudarenz@gmail.com>
2021-10-25 01:54:33 -0300

    util that return diffrence in minutes and esconds between two dates

--- src/utils.ts
+++ src/utils.ts
@@ -1,55 +1,68 @@
-import { quintOut } from 'svelte/easing';
-import { crossfade } from 'svelte/transition';
+import { quintOut } from "svelte/easing"
+import { crossfade } from "svelte/transition"
 
 export interface IColorRanges {
-  min: number,
-  max: number,
-  color: string,
+  min: number
+  max: number
+  color: string
   text_color: string
 }
 interface IColorConfig {
-  offer: Boolean,
-  agression: Boolean,
+  offer: Boolean
+  agression: Boolean
   ranges: Array<IColorRanges>
 }
 /** Return HH:MM:SS date from date object **/
-export const getFormatedTime = (date: Date) => date && ` ${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}:${String(date.getUTCSeconds()).padStart(2, "0")} `
+export const getFormatedTime = (date: Date) =>
+  date &&
+  ` ${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}:${String(date.getUTCSeconds()).padStart(
+    2,
+    "0"
+  )} `
 /** Checks if date is lower than other date **/
-export const dateIsLowerThan = (main_date: Date, date_to_compare: Date) => main_date < date_to_compare;
+export const dateIsLowerThan = (main_date: Date, date_to_compare: Date) => main_date < date_to_compare
 /** Get/Generate a id for each price **/
-export const getPriceID = (price: Number): string => 'price_' + price.toFixed(1).replace('.', '_')
+
+// get difference in mm:ss between two dates
+export function minutesAndSecondsDiff(date1, date2) {
+  let diff = date2.getTime() - date1.getTime()
+  let minutes = Math.floor(diff / (1000 * 60))
+  let seconds = Math.floor(diff / 1000) - minutes * 60
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
+}
+
+export const getPriceID = (price: Number): string => "price_" + price.toFixed(1).replace(".", "_")
 
 export const [send, receive] = crossfade({
-  duration: d => Math.sqrt(d * 200),
+  duration: (d) => Math.sqrt(d * 200),
   fallback(node, params) {
-    const style = getComputedStyle(node);
-    const transform = style.transform === 'none' ? '' : style.transform;
+    const style = getComputedStyle(node)
+    const transform = style.transform === "none" ? "" : style.transform
 
     return {
       duration: 150,
       easing: quintOut,
-      css: t => `
+      css: (t) => `
 					transform: ${transform} scale(${t});
 					opacity: ${t}
-				`
-    };
-  }
-});
-
+				`,
+    }
+  },
+})
 
 export const getQuantityColor = (quantity: Number, ranges: IColorRanges[]): IColorRanges => {
-  let found = ranges?.find(color => quantity >= color.min && quantity <= color.max)
+  let found = ranges?.find((color) => quantity >= color.min && quantity <= color.max)
   return found || null
 }
 
 export const groupBy = (objectArray, property) => {
   return objectArray.reduce((acc, obj) => {
-    const key = obj[property];
+    const key = obj[property]
     if (!acc[key]) {
-      acc[key] = [];
+      acc[key] = []
     }
     // Add object to list for given key's value
-    acc[key].push(obj);
-    return acc;
-  }, {});
+    acc[key].push(obj)
+    return acc
+  }, {})
 }
